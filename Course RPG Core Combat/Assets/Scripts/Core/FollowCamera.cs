fbe520689ecec72869ad898e52634e92f6d7fa16
Taencrash <taencrash@users.noreply.github.com>
2020-07-11 08:16:14 -0300

    Basic Movement
    Changed fixed target to point & click movement;
    Implemented 3rd Person Follower Camera;
    Imported Animation & Characters;
    Creation of Animation Controller and Blend Tree;
    Player animation changes depending on the local velocity;
    Project folders organization;

---
+++ Course RPG Core Combat/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform _target = null;
+
+    void LateUpdate()
+    {
+        transform.position = _target.position;
+    }
+}
---
+++ Course RPG Core Combat/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent _playerNavMeshAgent = null;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        
+        if(hasHit)
+        {
+            _playerNavMeshAgent.destination = hit.point;
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("Forward Speed", speed);
+
+        Debug.Log(velocity + " / " + localVelocity);
+    }
+}
+
+// Ray lastRay;
+// Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
